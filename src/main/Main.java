1d1f4f341f3081068aa3aeaa46f095307cd92e21
Kannen Joshi <22418234+InfernoKay@users.noreply.github.com>
2021-02-20 17:32:04 +0000

    Initial Setup of Main Class and Create Window
    Created Main class extending the JavaFX application.
    Initialises JFX.
    Sets the window title to Clue!

--- src/main/Main.java
+++ src/main/Main.java
@@ -14,3 +14,18 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static voic main(String[] args) {
+        // Initialise as JavaFX project
+        launch(args);
+    }
+
+    // Overrides the JFX Start function
+    // Start is run after being initialised
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Clue!");
+    }
+}

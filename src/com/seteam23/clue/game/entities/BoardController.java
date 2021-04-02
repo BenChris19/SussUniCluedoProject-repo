76319360063d49ac2d05194618f0320663fc80b3
BenChris19 <78378072+BenChris19@users.noreply.github.com>
2021-04-02 18:36:57 +0100

    Update BoardController.java

--- src/com/seteam23/clue/game/entities/BoardController.java
+++ src/com/seteam23/clue/game/entities/BoardController.java
@@ -6,13 +6,22 @@
 package com.seteam23.clue.game.entities;
 
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -26,6 +35,10 @@ public class BoardController implements Initializable {
     @FXML private AnchorPane anchor_pane;
     @FXML private ImageView background_img;
     @FXML private GridPane grid;
+    @FXML private ImageView player_img;
+    
+    @FXML private Button dices;
+    @FXML private Button suggAcc;
     
     /**
      * Initializes the controller class.
@@ -35,6 +48,19 @@ public class BoardController implements Initializable {
         board = new Board(this);
         createButtons();
     }
+    @FXML
+    private void change(ActionEvent event) throws Exception{ 
+        Button b = (Button)event.getSource();
+        if (b.getText().equals("Make Suggestion")){
+                    b.setStyle("-fx-background-color: red");
+                    b.setText("Make Accusation"); 
+        }
+        else{
+                    b.setStyle("-fx-background-color: green");
+                    b.setText("Make Suggestion"); 
+        }
+
+    }
     
     /**
      * Change the background image to the designated new image
@@ -43,6 +69,43 @@ public class BoardController implements Initializable {
     public void changeBackground(String image_path){
         background_img.setImage(new Image(getClass().getResource(image_path).toExternalForm()));
     }
+    public void changeChar(String image_path){
+        player_img.setImage(new Image(getClass().getResource(image_path).toExternalForm()));
+    }
+    @FXML
+    private void rollDices(ActionEvent event) throws Exception{
+        Random r = new Random();
+        int die1 = r.nextInt(6)+1;
+        int die2 = r.nextInt(6)+1;
+        
+        Stage window = new Stage();
+            window.initModality(Modality.APPLICATION_MODAL);
+            window.setWidth(250);
+            window.setHeight(150);
+            
+            BorderPane paneRoll = new BorderPane();
+            Label labelRoll = new Label("You rolled a...");
+            Label diceSum = new Label(Integer.toString(die1 + die2));
+            BorderPane.setAlignment(labelRoll, Pos.TOP_CENTER);
+            BorderPane.setAlignment(diceSum,Pos.CENTER);
+            paneRoll.setTop(labelRoll);
+            paneRoll.setCenter(diceSum);
+            
+            Button buttonOK = new Button("Ok");  //closes current window and opens a new one, reseting the game
+            buttonOK.setOnAction(e->{
+                window.close();
+            });
+            BorderPane.setAlignment(buttonOK,Pos.CENTER);
+            paneRoll.setBottom(buttonOK);
+        
+            Scene scene = new Scene(paneRoll);
+            window.setScene(scene);
+            window.showAndWait();
+
+        
+    }
+
+    
     
     /**
      * Get the button linked to each tile and add it to the GridPane

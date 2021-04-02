41a8542f6c81521cd773d4f39c45050edb7f0bf4
BenChris19 <78378072+BenChris19@users.noreply.github.com>
2021-04-02 18:55:19 +0100

    Update Game.java

--- src/com/seteam23/clue/game/Game.java
+++ src/com/seteam23/clue/game/Game.java
@@ -18,44 +18,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Game{
+    Board board;
     List<Card> cards;
     public Game() throws IOException{
         cards = new ArrayList<>();
         initialise();
     }
-    
+
 
 
     /**
      * Loads data from the init.txt file in the directory in to the game object
-     * 
-     * @throws IOException
-     * 
+@@ -32,7 +34,7 @@ public Game() throws IOException{
      */
     public void initialise() throws IOException{
         //Creates file reader
+        try (BufferedReader br = new BufferedReader(new FileReader("init.txt"))) {
         try (BufferedReader br = new BufferedReader(new FileReader("src/resources/main/init.csv"))) {
             String line;
             //Reads through each line in the document
             while ((line = br.readLine()) != null) {
-                //Filters out comments in the file starting with character #
-                if (!(line.charAt(0) == '#')){
-                    //Splits a line in to strings separated by commas
-                    String[] info = line.split(",");
-                    switch (info[0]){
-                        case "CARD":
-                            //Creates a card object with specified data
-                            String tempName = info[2];
-                            String tempPath = info[3];
-                            String tempType = info[1];
-                            Card temp = new Card(tempName, tempPath, tempType);
-                            //Adds card to game
-                            cards.add(temp);
-                            break;
-                    }
-                }
-            }
-        //Thrown if file not found
+@@ -57,5 +59,8 @@ public void initialise() throws IOException{
         } catch (IOException e) {
             System.out.println("init.txt missing from directory");
         }
